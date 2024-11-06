3e8f67dd67ac5d2359bd36a0623064d9760eb449
J. Miguel Aguario Diaz <105244825+MkeBssUni@users.noreply.github.com>
2024-11-06 14:10:05 -0600

    añadí un archivo de configuración de dio, incluye interceptors y así

--- DIO/flutter_dio/lib/app/config/app_config.dart
+++ DIO/flutter_dio/lib/app/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String baseUrl = 'http://localhost:8080/api/resourceTypes/';
+  static const String baseUrl = 'http://10.0.2.2:8080/api/resourceTypes/';
   static const String email = 'baluevents24@gmail.com';
   static const String password = 'BaluEvents24';
 }
